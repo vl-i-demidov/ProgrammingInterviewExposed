047f7b92dd0cbcd15f55d48eb64b7b800b536ab4
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-07 14:40:49 +0300

    Null or Cycle (iter. 1)

---
+++ ProgrammingInterviewExposed/LinkedList/NullOrCycle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.LinkedList
+{
+    /*
+     PROBLEM You are given a linked list with at least one node that is either null terminated (acyclic).
+     Write a function that takes a pointer to the head of a list and determines whether
+     the list is cyclic or acyclic. Your function should return false if the list is acyclic
+     and true if it is cyclic. You may not modify the list in any way.
+     */
+
+    public class NullOrCycleResolver
+    {
+        //O(n^2)
+        public bool IsCyclicList<T>(LinkedListElement<T> head)
+        {
+            if (head.Next == head)
+            {
+                return true;
+            }
+
+            var n = head;
+            while (n.Next != null)
+            {
+                var checkNode = head;
+                while (checkNode != n)
+                {
+                    if (n.Next == checkNode)
+                    {
+                        return true;
+                    }
+                    checkNode = checkNode.Next;
+                }
+                n = n.Next;
+            }
+
+            return false;
+        }
+    }
+
+}
---
+++ ProgrammingInterviewExposed/LinkedList/NullOrCycleTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProgrammingInterviewExposed.LinkedList
+{
+    public class NullOrCycleTests
+    {
+        [Fact]
+        public void NullDeterminedRight()
+        {
+            var n1 = new LinkedListElement<int>(1);
+            var n2 = new LinkedListElement<int>(1);
+            var n3 = new LinkedListElement<int>(1);
+            var n4 = new LinkedListElement<int>(1);
+            n1.SetNext(n2);
+            n2.SetNext(n3);
+            n3.SetNext(n4);
+
+            var ncr = new NullOrCycleResolver();
+            Assert.False(ncr.IsCyclicList(n1));
+        }
+
+        [Fact]
+        public void CycleDeterminedRight()
+        {
+            var n1 = new LinkedListElement<int>(1);
+            var n2 = new LinkedListElement<int>(1);
+            var n3 = new LinkedListElement<int>(1);
+            var n4 = new LinkedListElement<int>(1);
+            n1.SetNext(n2);
+            n2.SetNext(n3);
+            n3.SetNext(n4);
+            n4.SetNext(n2);
+
+            var ncr = new NullOrCycleResolver();
+            Assert.True(ncr.IsCyclicList(n1));
+        }
+
+    }
+}
