a77234b22b20571c247eb4967e82873c7892d226
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-07 17:26:11 +0300

    Height of a Tree

---
+++ ProgrammingInterviewExposed/TreesAndGraphs/HeightOfATree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.TreesAndGraphs
+{
+    /*
+     PROBLEM The height of a tree (binary or not) is defined to be the maximum
+     distance from the root node to any leaf node.
+     Write a function to calculate the height of an arbitrary binary tree.
+     */
+
+
+    public class Node
+    {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public bool IsLeaf => Left == null && Right == null;
+    }
+
+    public class HeightCalculator
+    {
+        //O(n)
+        public int CalcTreeHeightBinaryTree(Node root)
+        {
+            int rightDepth = root.Right != null ? CalcTreeHeightBinaryTree(root.Right) + 1 : 1;
+            int leftDepth = root.Left != null ? CalcTreeHeightBinaryTree(root.Left) + 1 : 1;
+
+            return Math.Max(rightDepth, leftDepth);
+        }
+
+    }
+
+}
---
+++ ProgrammingInterviewExposed/TreesAndGraphs/HeightOfATreeTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProgrammingInterviewExposed.TreesAndGraphs
+{
+    public class HeightOfATreeTests
+    {
+        [Fact]
+        public void CalculateHeightRight()
+        {
+            var root = new Node()
+            {
+                Left = new Node()
+                {
+                    Left = new Node()
+                    {
+                        Left = new Node(),
+                        Right = new Node()
+                        {
+                            Right = new Node()
+                        },
+                    },
+                    Right = new Node(),
+                },
+                Right = new Node()
+                {
+                    Left = new Node(),
+                    Right = new Node(),
+                }
+            };
+
+            var calc = new HeightCalculator();
+            Assert.Equal(5, calc.CalcTreeHeightBinaryTree(root));
+
+        }
+    }
+}
