956125dbbf21781cee61701044f2ac164c0e7ac5
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-07 13:26:54 +0300

    Unflattening List (iter. 1)

---
+++ ProgrammingInterviewExposed/LinkedList/ListUnflattening.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.LinkedList
+{
+    /*
+     PROBLEM Unflatten the list created by the previous problem and restore the
+     data structure to its original condition.
+     */
+
+    public class ListUnflattener
+    {
+        //restoring initial state from flattened listed
+        //as implemented in ListFlattener.FlattenList is impossible
+
+
+        public void UnflattenList(Node head)
+        {
+            var n = head;
+            while (n != null)
+            {
+                if (n.Child != null)
+                {
+                    //terminate the list before the child
+                    n.Child.Previous.Next = null;
+                    n.Child.Previous = null;
+                    UnflattenList(n.Child);
+                }
+
+                n = n.Next;
+            }
+        }
+
+
+        public Node FlattenList(Node head, Node tail)
+        {
+            var n = head;
+            while (n != null)
+            {
+                if (n.Child != null)
+                {
+                    Append(n.Child, ref tail);
+                }
+                n = n.Next;
+            }
+            return head;
+        }
+
+        private void Append(Node child, ref Node tail)
+        {
+            Node curNode;
+            tail.Next = child;
+            child.Previous = tail;
+
+            for (curNode = child; curNode.Next != null; curNode = curNode.Next)
+            {
+            }
+
+            tail = curNode;
+        }
+    }
+}
---
+++ ProgrammingInterviewExposed/LinkedList/ListUnflatteningTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ProgrammingInterviewExposed.LinkedList
+{
+    public class ListUnflatteningTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public ListUnflatteningTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void UnflattensRight()
+        {
+            var n1l1 = new Node(11);
+            var n2l1 = new Node(12);
+            var n3l1 = new Node(13);
+            var n1l2 = new Node(21);
+            var n2l2 = new Node(22);
+            var n1l3 = new Node(31);
+            var n1l4 = new Node(41);
+
+            n1l1.Next = n2l1;
+            n2l1.Previous = n1l1;
+            n2l1.Next = n3l1;
+            n3l1.Previous = n2l1;
+
+            n2l1.Child = n1l2;
+
+            n1l2.Next = n2l2;
+            n2l2.Previous = n1l2;
+
+            n1l2.Child = n1l3;
+
+            n1l3.Child = n1l4;
+
+            PrintNode(n1l1);
+
+            var fl = new ListUnflattener();
+            fl.FlattenList(n1l1, n3l1);
+
+            PrintNode(n1l1);
+
+            fl.UnflattenList(n1l1);
+
+            PrintNode(n1l1);
+        }
+
+
+        private void PrintNode(Node head, bool printEnd = true)
+        {
+            var print = "";
+            var parents = new List<Node>();
+
+            var n = head;
+            while (n != null)
+            {
+                if (n.Child != null)
+                {
+                    parents.Add(n);
+                }
+
+                print += $"{n.Value}\t";
+                n = n.Next;
+            }
+
+            _output.WriteLine(print);
+
+            foreach (var pn in parents)
+            {
+                _output.WriteLine($"({pn.Value}) is parent of:");
+                PrintNode(pn.Child, printEnd: false);
+            }
+
+            if (printEnd)
+            {
+                _output.WriteLine(new string('-', 20));
+            }
+        }
+    }
+}
