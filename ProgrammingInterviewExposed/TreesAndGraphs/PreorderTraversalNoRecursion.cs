6fb6c838ded452efd92478018fe78c6f984dd898
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-08 09:57:23 +0300

    Preorder Traversal, No Recursion (iter. 1)

---
+++ ProgrammingInterviewExposed/TreesAndGraphs/PreorderTraversalNoRecursion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.TreesAndGraphs
+{
+    /*
+     PROBLEM Perform a preorder traversal of a binary search tree, printing the
+     value of each node, but this time you may not use recursion.
+     */
+
+
+    class NoRecursionPreorderNodePrinter
+    {
+        public string Print(Node root)
+        {
+            var sb = new StringBuilder();
+            Action<Node> printVals = n => sb.Append($"{n.Value} ");
+
+            Traverse(root, printVals);
+
+            return sb.ToString();
+        }
+
+        private void Traverse(Node root, Action<Node> action)
+        {
+            var node = root;
+            Stack<Node> toProcess = new Stack<Node>();
+
+            while (true)
+            {
+                action(node);
+                if (node.Left != null)
+                {
+                    if (node.Right != null)
+                    {
+                        toProcess.Push(node.Right);
+                    }
+                    node = node.Left;
+                }
+                else if (node.Right != null)
+                {
+                    node = node.Right;
+                }
+                else if (toProcess.Count > 0)
+                {
+                    node = toProcess.Pop();
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ ProgrammingInterviewExposed/TreesAndGraphs/PreorderTraversalNoRecursionTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ProgrammingInterviewExposed.TreesAndGraphs
+{
+    public class PreorderTraversalNoRecursionTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public PreorderTraversalNoRecursionTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void ValuesArePrintedRight()
+        {
+            var root = new Node(100)
+            {
+                Left = new Node(50)
+                {
+                    Left = new Node(25),
+                    Right = new Node(75),
+                },
+                Right = new Node(150)
+                {
+                    Left = new Node(125)
+                    {
+                        Left = new Node(110)
+                    },
+                    Right = new Node(175),
+                },
+            };
+
+            var printer = new NoRecursionPreorderNodePrinter();
+            _output.WriteLine(printer.Print(root));
+        }
+    }
+}
