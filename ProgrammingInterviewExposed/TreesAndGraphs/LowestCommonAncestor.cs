abc860eaa739db2097f1e1f71b10d93faf81cc12
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-08 11:27:10 +0300

    Lowest Common Ancestor

---
+++ ProgrammingInterviewExposed/TreesAndGraphs/LowestCommonAncestor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.TreesAndGraphs
+{
+    /*
+     PROBLEM Given the value of two nodes in a binary search tree, find the lowest
+     (nearest) common ancestor. You may assume that both values already exist in
+     the tree.
+     */
+
+    class CommonAncestorResolver
+    {
+        //O(log(n))
+        public Node FindLowest(Node root, int valueA, int valueB)
+        {
+            int max = Math.Max(valueA, valueB);
+            int min = Math.Min(valueA, valueB);
+            var node = root;
+
+            while (node != null)
+            {
+                var rootValue = node.Value;
+                if (rootValue >= min && rootValue < max)
+                {
+                    return node;
+                }
+
+                if (min < rootValue)
+                {
+                    node = node.Left;
+                }
+                else
+                {
+                    node = node.Right;
+                }
+            }
+            return null;
+        }
+
+        public Node FindLowestRecursive(Node root, int valueA, int valueB)
+        {
+            int max = Math.Max(valueA, valueB);
+            int min = Math.Min(valueA, valueB);
+            var node = root;
+
+            GetNextLowestRecursive(ref node, min, max);
+            return node;
+        }
+
+        private void GetNextLowestRecursive(ref Node root, int min, int max)
+        {
+            if (root.Value >= min && root.Value < max)
+            {
+                return;
+            }
+
+            root = min < root.Value ? root.Left : root.Right;
+            GetNextLowestRecursive(ref root, min, max);
+        }
+    }
+}
---
+++ ProgrammingInterviewExposed/TreesAndGraphs/LowestCommonAncestorTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ProgrammingInterviewExposed.TreesAndGraphs
+{
+    public class LowestCommonAncestorTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public LowestCommonAncestorTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void LowestCommonAncestorFoundRight()
+        {
+            var root = new Node(20)
+            {
+                Left = new Node(8)
+                {
+                    Left = new Node(4),
+                    Right = new Node(12)
+                    {
+                        Left = new Node(10),
+                        Right = new Node(14)
+                    }
+                },
+                Right = new Node(22)
+            };
+
+            var resolver = new CommonAncestorResolver();
+            var ancestor = resolver.FindLowest(root, 4, 14);
+            Assert.NotNull(ancestor);
+            Assert.Equal(8, ancestor.Value);
+        }
+
+        [Fact]
+        public void LowestCommonAncestorFoundRightRecursive()
+        {
+            var root = new Node(20)
+            {
+                Left = new Node(8)
+                {
+                    Left = new Node(4),
+                    Right = new Node(12)
+                    {
+                        Left = new Node(10),
+                        Right = new Node(14)
+                    }
+                },
+                Right = new Node(22)
+            };
+
+            var resolver = new CommonAncestorResolver();
+            var ancestor = resolver.FindLowestRecursive(root, 4, 14);
+            Assert.NotNull(ancestor);
+            Assert.Equal(8, ancestor.Value);
+        }
+    }
+}
