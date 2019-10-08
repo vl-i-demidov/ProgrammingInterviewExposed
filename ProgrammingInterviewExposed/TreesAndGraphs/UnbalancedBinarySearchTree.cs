ee1572022441f50d55cf8025a6e6d11b48cd91e6
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-08 15:34:53 +0300

    Unabalanced Binary search tree (iter. 1)

---
+++ ProgrammingInterviewExposed/TreesAndGraphs/UnbalancedBinarySearchTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.TreesAndGraphs
+{
+    /*
+     PROBLEM Given an unbalanced binary search tree with more nodes in the left
+     subtree than the right, reorganize the tree to improve its balance while maintaining
+     the properties of a binary search tree
+     */
+
+    class TreeBalancer
+    {
+        //O(n) runtime, O(n) space
+        public Node BalanceTree(Node root)
+        {
+            var nodes = new List<Node>();
+            TraverseOrder(root, n => nodes.Add(n));
+            var nodeArray = nodes.ToArray();
+
+            return BalanceTreeArray(nodeArray, 0, nodeArray.Length - 1);
+        }
+
+        private void TraverseOrder(Node root, Action<Node> action)
+        {
+            if (root.Left != null)
+            {
+                action(root.Left);
+            }
+
+            action(root);
+
+            if (root.Right != null)
+            {
+                action(root.Right);
+            }
+        }
+
+        private Node BalanceTreeArray(Node[] array, int startIndex, int endIndex)
+        {
+            //binary search tree
+
+            int middleIndex = (int)Math.Ceiling((endIndex + startIndex) / 2.0);
+
+            var node = array[middleIndex];
+            node.Left = null;
+            node.Right = null;
+
+            if (startIndex == endIndex)
+            {
+                return node;
+            }
+
+            node.Left = BalanceTreeArray(array, startIndex, middleIndex - 1);
+
+            if (endIndex > middleIndex)
+            {
+                node.Right = BalanceTreeArray(array, middleIndex + 1, endIndex);
+            }
+
+            return node;
+        }
+    }
+}
---
+++ ProgrammingInterviewExposed/TreesAndGraphs/UnbalancedBinarySearchTreeTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProgrammingInterviewExposed.TreesAndGraphs
+{
+    public class UnbalancedBinarySearchTreeTests
+    {
+        [Fact]
+        public void BalancesRight()
+        {
+            var root = new Node(6)
+            {
+                Left = new Node(4)
+                {
+                    Left = new Node(2)
+                    {
+                        Left = new Node(1)
+                    },
+                    Right = new Node(3)
+                },
+                Right = new Node(7)
+            };
+
+            TreeBalancer balancer = new TreeBalancer();
+            var balancedRoot = balancer.BalanceTree(root);
+
+            AssertIsBinarySearchTree(balancedRoot);
+
+        }
+
+
+        private void AssertIsBinarySearchTree(Node root)
+        {
+            if (root.Left != null)
+            {
+                Assert.True(root.Value >= root.Left.Value);
+                AssertIsBinarySearchTree(root.Left);
+            }
+
+            if (root.Right != null)
+            {
+                Assert.True(root.Value < root.Right.Value);
+                AssertIsBinarySearchTree(root.Right);
+            }
+        }
+
+    }
+}
