9422ad50fc7ea0824eb7a3e83ace8a669fb3175d
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-10 10:41:42 +0300

    Reverse Words (iter. 2)

--- ProgrammingInterviewExposed/ArrayAndString/ReverseWords.cs
+++ ProgrammingInterviewExposed/ArrayAndString/ReverseWords.cs
@@ -47,7 +47,123 @@ namespace ProgrammingInterviewExposed.ArrayAndString
 
             return reverse.ToString();
         }
+
+        //Walking from the end of source string
+        //when we find the word's end, copy word to a buffer
+        public static string ReverseGeneral(string sentence)
+        {
+            var chars = sentence.ToCharArray();
+            var buffer = new char[chars.Length];
+
+            int bufferPos = 0;
+            int tokenReadPos = chars.Length - 1;
+
+            while (tokenReadPos >= 0)
+            {
+                if (chars[tokenReadPos] == ' ')
+                {
+                    buffer[bufferPos++] = chars[tokenReadPos--];
+                }
+                else
+                {
+                    int wordEnd = tokenReadPos;
+                    while (tokenReadPos >= 0 && chars[tokenReadPos] != ' ')
+                    {
+                        tokenReadPos--;
+                    }
+
+                    int wordStart = tokenReadPos + 1;
+
+                    while (wordEnd >= wordStart)
+                    {
+                        buffer[bufferPos] = chars[wordStart];
+                        bufferPos++;
+                        wordStart++;
+                    }
+                }
+            }
+
+            return new string(buffer);
+        }
+
+
+        //Reversing string in place (use same char array as source string)
+        //More useful in C/C++
+        public static string Reverse2(string sentence)
+        {
+            var chars = sentence.ToCharArray();
+
+            //1. Reverse all letters, so that "Hello, World!" becomes "!dlroW ,olleH"
+            Reverse(chars, 0, chars.Length - 1);
+
+            //2. Reverse each word separately: "!dlroW ,olleH" -> "World! Hello,"
+
+            int start = 0, end = 0;
+
+            while (end <= chars.Length)
+            {
+                if (end == chars.Length || chars[end] == ' ')
+                {
+                    Reverse(chars, start, end - 1);
+                    start = end + 1;
+                }
+                end++;
+            }
+
+            return new string(chars);
+        }
+
+        //Reversing string in place
+        //almost the same as Reverse2
+        public static string Reverse3(string sentence)
+        {
+            var chars = sentence.ToCharArray();
+
+            //1. Reverse all letters, so that "Hello, World!" becomes "!dlroW ,olleH"
+            Reverse(chars, 0, chars.Length - 1);
+
+            //2. Reverse each word separately: "!dlroW ,olleH" -> "World! Hello,"
+
+            int end = 0;
+
+            while (end < chars.Length)
+            {
+                if (chars[end] != ' ')
+                {
+                    var start = end;
+
+                    while (end < chars.Length && chars[end] != ' ')
+                    {
+                        end++;
+                    }
+
+                    end--;
+
+                    Reverse(chars, start, end);
+                }
+
+                end++;
+            }
+
+            return new string(chars);
+        }
+
+        private static void Reverse(char[] str, int start, int end)
+        {
+            while (start < end)
+            {
+                var temp = str[start];
+                str[start] = str[end];
+                str[end] = temp;
+
+                start++;
+                end--;
+            }
+        }
     }
+
+
+
 
 }
 
--- ProgrammingInterviewExposed/ArrayAndString/ReverseWordsTests.cs
+++ ProgrammingInterviewExposed/ArrayAndString/ReverseWordsTests.cs
@@ -7,14 +7,31 @@ namespace ProgrammingInterviewExposed.ArrayAndString
 {
     public class ReverseWordsTests
     {
+        const string Sentence = "Do or do not, there is no try.";
+        const string ExpectedResult = "try. no is there not, do or Do";
+
         [Fact]
         public void ReversesRight()
         {
-            var sentence = "Do or do not, there is no try.";
-            var expectedResult = "try. no is there not, do or Do";
-
-            Assert.Equal(expectedResult, ReverseWords.Reverse(sentence));
+            Assert.Equal(ExpectedResult, ReverseWords.Reverse(Sentence));
         }
 
+        [Fact]
+        public void ReversesRightGeneral()
+        {
+            Assert.Equal(ExpectedResult, ReverseWords.ReverseGeneral(Sentence));
+        }
+
+        [Fact]
+        public void ReversesRight2()
+        {
+            Assert.Equal(ExpectedResult, ReverseWords.Reverse2(Sentence));
+        }
+
+        [Fact]
+        public void ReversesRight3()
+        {
+            Assert.Equal(ExpectedResult, ReverseWords.Reverse3(Sentence));
+        }
     }
 }
