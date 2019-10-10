5536c43ae8da8598581f9a99c661bb4581d28020
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-10 14:30:54 +0300

    UTF8 String Validation (iter. 1)

---
+++ ProgrammingInterviewExposed/ArrayAndString/UTF8StringValidation.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.ArrayAndString
+{
+    /*
+     PROBLEM A valid UTF-8 string may contain only the following four bit
+     patterns:
+         0xxxxxxx
+         110xxxxx 10xxxxxx
+         1110xxxx 10xxxxxx 10xxxxxx
+         11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+     Write a function to determine whether a string meets this necessary (but not sufficient)
+     criterion for UTF-8 validity.
+     */
+
+    class UTF8StringValidation
+    {
+        public static bool IsValid(byte[] str)
+        {
+            var ba = new BitArray(str);
+
+            int pos = 0;
+
+            bool checkTail = false;
+            int tailsRemained = 0;
+
+            while (pos < ba.Length)
+            {
+                if (checkTail)
+                {
+                    if (ba[pos] && !ba[pos + 1])
+                    {
+                        tailsRemained--;
+                        if (tailsRemained == 0)
+                        {
+                            checkTail = false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    int onCount = 0;
+                    int i = pos;
+
+                    while (ba[i] && onCount < 5)
+                    {
+                        onCount++;
+                        i++;
+                    }
+
+                    if (onCount == 0)
+                    {
+                        //do nothing
+                    }
+                    else if (onCount >= 2 && onCount <= 4)
+                    {
+                        checkTail = true;
+                        tailsRemained = onCount - 1;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                pos += 8;
+            }
+
+            return true;
+        }
+
+
+    }
+}
---
+++ ProgrammingInterviewExposed/ArrayAndString/UTF8StringValidationTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProgrammingInterviewExposed.ArrayAndString
+{
+    public class UTF8StringValidationTests
+    {
+        [Fact]
+        public void StringValidatedRight()
+        {
+            Assert.True(UTF8StringValidation.IsValid(GetValidString()));
+            Assert.False(UTF8StringValidation.IsValid(GetInvalidString()));
+        }
+
+
+        private static byte[] GetValidString()
+        {
+            var raw = "00000000 11100000_10000000_10000000 11110000_10000000_10000000_10000000 11000000_10000000" +
+                      "11100000_10000000_10000000 11100000_10000000_10000000 00000000";
+            return ByteArrayFromRaw(raw);
+        }
+
+        private static byte[] GetInvalidString()
+        {
+            var raw = "00000000 11100000_10000000_10000000 11100000_10000000_10000000_10000000 1100000_10000000" +
+                      "11100000_10000000_10000000 11100000_10000000_10000000 00000000";
+            return ByteArrayFromRaw(raw);
+        }
+
+        private static byte[] ByteArrayFromRaw(string raw)
+        {
+            List<bool> data = new List<bool>();
+            foreach (var ch in raw)
+            {
+                if (Char.IsDigit(ch))
+                {
+                    data.Add(ch == '1');
+                }
+            }
+            
+            return BitArrayToByteArray(new BitArray(data.ToArray()));
+        }
+
+
+        private static byte[] BitArrayToByteArray(BitArray bits)
+        {
+            byte[] ret = new byte[(bits.Length - 1) / 8 + 1];
+            bits.CopyTo(ret, 0);
+            return ret;
+        }
+
+    }
+}
