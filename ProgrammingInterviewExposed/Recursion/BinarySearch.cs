e519bb3adff2478c4cfdb7af2b91c6e745383e18
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-10 16:29:21 +0300

    Binary Search (iter. 1)

---
+++ ProgrammingInterviewExposed/Recursion/BinarySearch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.Recursion
+{
+    /*
+      PROBLEM Implement a function to perform a binary search on a sorted array
+      of integers to find the index of a given integer. Comment on the efficiency of this
+      search, and compare it with other search methods.
+     */
+
+    class BinarySearch
+    {
+        public static int FindIndex(int[] array, int value)
+        {
+            if (array.Length == 0)
+            {
+                return -1;
+            }
+            return Find(array, 0, array.Length - 1, value);
+        }
+
+        private static int Find(int[] array, int startIndex, int endIndex, int value)
+        {
+            // xInt / yInt == Math.Floor(x.0 / y.0)
+
+            int midIndex = (startIndex + endIndex) / 2;
+            var midValue = array[midIndex];
+
+            //base case
+
+            if (midValue == value)
+            {
+                return midIndex;
+            }
+
+            if (startIndex == endIndex)
+            {
+                return -1;
+            }
+
+            //recursive case
+            if (midValue < value)
+            {
+                return Find(array, midIndex + 1, endIndex, value);
+            }
+            return Find(array, startIndex, midIndex + 1, value);
+
+        }
+
+    }
+}
---
+++ ProgrammingInterviewExposed/Recursion/BinarySearchTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProgrammingInterviewExposed.Recursion
+{
+    public class BinarySearchTests
+    {
+        [Fact]
+        public void SearchesRight()
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            int[] array2 = { 1, 2, 3, 4 };
+            int[] array3 = { };
+
+            Assert.Equal(2, BinarySearch.FindIndex(array, 3));
+            Assert.Equal(2, BinarySearch.FindIndex(array2, 3));
+            Assert.Equal(-1, BinarySearch.FindIndex(array, 7));
+            Assert.Equal(-1, BinarySearch.FindIndex(array3, 3));
+        }
+
+    }
+}
