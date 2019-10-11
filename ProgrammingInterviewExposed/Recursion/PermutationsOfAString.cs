93e8463581869ae3846eea0597d683142f9049a5
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-11 12:00:21 +0300

    Permutations Of a String (iter. 1)

---
+++ ProgrammingInterviewExposed/Recursion/PermutationsOfAString.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.Recursion
+{
+    /*
+      PROBLEM Implement a routine that prints all possible orderings of the characters
+      in a string. In other words, print all permutations that use all the characters
+      from the original string. For example, given the string “hat”, your function
+      should print the strings “tha”, “aht”, “tah”, “ath”, “hta”, and “hat”. Treat each
+      character in the input string as a distinct character, even if it is repeated. Given
+      the string “aaa”, your routine should print “aaa” six times. You may print the
+      permutations in any order you choose.
+    */
+
+    class PermutationsOfAString
+    {
+        private bool[] _used;
+        private char[] _inputWord;
+
+        private List<string> _out;
+        private StringBuilder _outWord;
+
+
+        public PermutationsOfAString(string str)
+        {
+            _inputWord = str.ToCharArray();
+            _out = new List<string>();
+            _outWord = new StringBuilder();
+        }
+
+        public List<string> GetPermutations()
+        {
+            _used = new bool[_inputWord.Length];
+            _out.Clear();
+            _outWord.Clear();
+
+            Permute();
+
+            return _out;
+        }
+
+        private void Permute()
+        {
+            if (_outWord.Length == _inputWord.Length)
+            {
+                _out.Add(_outWord.ToString());
+                return;
+            }
+
+            for (int ind = 0; ind < _inputWord.Length; ind++)
+            {
+                var ch = _inputWord[ind];
+                if (_used[ind]) continue;
+
+                _outWord.Append(_inputWord[ind]);
+                _used[ind] = true;
+
+                Permute();
+
+                _used[ind] = false;
+
+                _outWord.Length = _outWord.Length - 1;
+            }
+        }
+    }
+}
---
+++ ProgrammingInterviewExposed/Recursion/PermutationsOfAStringTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ProgrammingInterviewExposed.Recursion
+{
+    public class PermutationsOfAStringTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public PermutationsOfAStringTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void PermutatesStringRight()
+        {
+            var program = new PermutationsOfAString("hat");
+
+            var permutations = program.GetPermutations();
+
+            _output.WriteLine(String.Join(" | ", permutations));
+
+            var unique = new HashSet<string>(permutations);
+
+            Assert.Equal(24, permutations.Count);
+            Assert.Equal(24, unique.Count);
+        }
+
+    }
+}
