643a61b843335a6f8ccf752aa57d5b8c07ca9833
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-15 17:36:05 +0300

    Multi-Key Sort (iter. 1)

---
+++ ProgrammingInterviewExposed/Sorting/MultiKeySort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.Sorting
+{
+    /*
+      PROBLEM You have an array of objects, each of which represents an employee:
+       public class Employee {
+           public String extension;
+           public String givenname;
+           public String surname;
+       }
+      Using a standard library sorting routine, sort the array so it is ordered alphabetically
+      by surname and then by given name as in a company phone book.
+     */
+
+    class MultiKeySort
+    {
+        private static GivenNameComparer _givenNameComparer = new GivenNameComparer();
+
+        //O(n*log(n)) + O(n) + O(l*k*log(k)) 
+        //(l - count of same given name groups, k - average count of items in the group)
+        public static void Sort(Employee[] data)
+        {
+            Array.Sort(data, SurnameComparison);
+
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                int start = i;
+                int end = i + 1;
+
+                while (end < data.Length && SurnameComparison(data[start], data[end]) == 0)
+                {
+                    end++;
+                }
+
+                if (end - start > 1)
+                {
+                    Array.Sort(data, start, end - start, _givenNameComparer);
+                    i = end - 1;
+                }
+            }
+        }
+
+        private static int SurnameComparison(Employee a, Employee b)
+        {
+            return a.Surname.CompareTo(b.Surname);
+        }
+
+        private class GivenNameComparer : IComparer<Employee>
+        {
+            public int Compare(Employee x, Employee y)
+            {
+                return x.GivenName.CompareTo(y.GivenName);
+            }
+        }
+    }
+
+    class Employee
+    {
+        public string Extension { get; set; }
+        public string GivenName { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ ProgrammingInterviewExposed/Sorting/MultiKeySortTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+
+namespace ProgrammingInterviewExposed.Sorting
+{
+    public class MultiKeySortTests
+    {
+        [Fact]
+        public void SortsRight()
+        {
+            var data = new[]
+            {
+                new Employee
+                {
+                    Surname="S3",
+                    GivenName="G2"
+                },
+                new Employee
+                {
+                    Surname="S3",
+                    GivenName="G3"
+                },
+                new Employee
+                {
+                    Surname="S3",
+                    GivenName="G1"
+                },
+                new Employee
+                {
+                    Surname="S2",
+                    GivenName="G2"
+                },
+                new Employee
+                {
+                    Surname="S1",
+                    GivenName="G1"
+                },
+            };
+
+            MultiKeySort.Sort(data);
+
+            var expSurnames = new[] { "S1", "S2", "S3", "S3", "S3" };
+            var expGivenNames = new[] { "G1", "G2", "G1", "G2", "G3" };
+
+            Assert.Equal(expSurnames, data.Select(e => e.Surname).ToArray());
+            Assert.Equal(expGivenNames, data.Select(e => e.GivenName).ToArray());
+        }
+    }
+}
