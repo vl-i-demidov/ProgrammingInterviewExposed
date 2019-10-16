0cf82f3882b3a708c1cf876fa78912cb0ad70fb3
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-16 13:25:59 +0300

    Optimized Quicksort

---
+++ ProgrammingInterviewExposed/Sorting/OptimizedQuicksort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.Sorting
+{
+    /*
+      PROBLEM Implement an efficient, in-place version of the quicksort algorithm.
+     */
+    class OptimizedQuicksort
+    {
+        public static void Sort(int[] data)
+        {
+
+        }
+
+        //to make algorithm in-place, we use swapping
+        //after calculating pivot value, all values to the left of pivot, that are greater than pivot,
+        //swapped with values to the right of pivot, that are less than pivot   
+        private static void Sort(int[] data, int left, int right)
+        {
+            int pivotValue = data[(left + right) / 2];
+            int i = left, j = right;
+
+            while (i < j)
+            {
+                // Find leftmost value greater than or equal to the pivot
+                while (data[i] < pivotValue) i++;
+
+                // Find rightmost value less than or equal to the pivot.
+                while (data[j] > pivotValue) j--;
+
+                if (i <= j)
+                {
+                    Swap(data, i, j);
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j)
+            {
+                Sort(data, left, j);
+            }
+
+            if (i > right)
+            {
+                Sort(data, i, right);
+            }
+        }
+
+        private static void Swap(int[] data, int srcInd, int destInd)
+        {
+            var temp = data[srcInd];
+            data[srcInd] = temp;
+            data[destInd] = temp;
+        }
+
+    }
+}
