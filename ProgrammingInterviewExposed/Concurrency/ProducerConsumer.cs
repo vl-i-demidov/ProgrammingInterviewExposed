47dc2342f94a5b2820319e8d31910bbcd891f4d7
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-22 10:16:11 +0300

    Producer/Consumer

---
+++ ProgrammingInterviewExposed/Concurrency/ProducerConsumer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProgrammingInterviewExposed.Concurrency
+{
+    /*
+      PROBLEM Write a producer thread and a consumer thread that share a fixedsize
+      buffer and an index to access the buffer. The producer should place numbers
+      into the buffer, and the consumer should remove the numbers. The order in
+      which the numbers are added or removed is not important.
+     */
+
+
+    class NumBuffer
+    {
+        private int _index = -1;
+        private readonly int[] _buffer;
+        private readonly object _lock = new object();
+
+        public NumBuffer(int size)
+        {
+            _buffer = new int[size];
+        }
+
+        public void AddNum(int n)
+        {
+            try
+            {
+                Monitor.Enter(_lock);
+
+                while (_index >= _buffer.Length - 1)
+                {
+                    Monitor.Wait(_lock);
+                }
+
+                _index++;
+                _buffer[_index] = n;
+
+                Monitor.PulseAll(_lock);
+            }
+            finally
+            {
+                Monitor.Exit(_lock);
+            }
+        }
+
+        public int Remove()
+        {
+            try
+            {
+                Monitor.Enter(_lock);
+
+                while (_index < 0)
+                {
+                    Monitor.Wait(_lock);
+                }
+
+                int n = _buffer[_index];
+                _index--;
+
+                Monitor.PulseAll(_lock);
+
+                return n;
+            }
+            finally
+            {
+                Monitor.Exit(_lock);
+            }
+        }
+
+    }
+
+    class ProducerConsumer
+    {
+        private readonly NumBuffer _buffer;
+        private readonly int _numCount;
+
+        public ConcurrentBag<int> Consumed { get; private set; }
+
+        public ProducerConsumer(int size, int numCount)
+        {
+            _buffer = new NumBuffer(size);
+            _numCount = numCount;
+
+            Consumed = new ConcurrentBag<int>();
+        }
+
+
+        public Task StartProduce()
+        {            
+           return Task.Run(()=> {
+                for (int i = 0; i < _numCount; i++)
+                {
+                    _buffer.AddNum(i);
+                }
+            });
+        }
+
+        public void StartConsume()
+        {
+            Task.Run(()=> {
+
+                while (true)
+                {
+                    int n=_buffer.Remove();
+                    Consumed.Add(n);
+                }
+
+            });
+        }
+
+    }
+}
---
+++ ProgrammingInterviewExposed/Concurrency/ProducerConsumerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProgrammingInterviewExposed.Concurrency
+{
+    public class ProducerConsumerTests
+    {
+        [Fact]
+        public void ConsumesAllProducedNums()
+        {
+            const int bufferSize = 100;
+            const int numCount = bufferSize * 100000;
+            var pc = new ProducerConsumer(bufferSize, numCount);
+
+            pc.StartConsume();
+            var produceTask = pc.StartProduce();
+
+            produceTask.Wait();
+            Task.Delay(1000).Wait();
+
+
+            Assert.Equal(numCount, pc.Consumed.Count);
+            Assert.Equal(numCount, pc.Consumed.Distinct().Count());
+        }
+    }
+}
