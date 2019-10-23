29572bf4f70d4c06ec493ee1369cb9258f6b92e7
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-23 10:00:28 +0300

    Number of Ones (iter. 1)

---
+++ ProgrammingInterviewExposed/GraphicsAndBitManipulation/NumberOfOnes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.GraphicsAndBitManipulation
+{
+    /*
+      PROBLEM Write a function that determines the number of 1 bits in the binary
+      representation of a given integer.
+     */
+
+    class NumberOfOnes
+    {
+        public static int GetOnBitsCount(int i)
+        {
+            int count = 0;
+
+            bool isNegative = false;
+            if (i < 0)
+            {
+                isNegative = true;
+                i = i ^ -1;
+            }
+
+            while (i != 0)
+            {
+                if ((i & 1) == 1)
+                {
+                    count++;
+                }
+                i = i >> 1;
+            }
+
+            if (isNegative)
+            {
+                count = 32 - count;
+            }
+
+            return count;
+        }
+
+
+    }
+}
---
+++ ProgrammingInterviewExposed/GraphicsAndBitManipulation/NumberOfOnesTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProgrammingInterviewExposed.GraphicsAndBitManipulation
+{
+    public class NumberOfOnesTests
+    {
+        [Fact]
+        public void CalculatesNumberRight()
+        {
+            Assert.Equal(0, NumberOfOnes.GetOnBitsCount(0));
+            Assert.Equal(1, NumberOfOnes.GetOnBitsCount(1));
+            Assert.Equal(1, NumberOfOnes.GetOnBitsCount(2));
+            Assert.Equal(2, NumberOfOnes.GetOnBitsCount(3));
+
+            Assert.Equal(1, NumberOfOnes.GetOnBitsCount(8));
+            Assert.Equal(1, NumberOfOnes.GetOnBitsCount(1024));
+            Assert.Equal(8, NumberOfOnes.GetOnBitsCount(2019));
+
+            Assert.Equal(25, NumberOfOnes.GetOnBitsCount(-2019));
+
+
+        }
+    }
+}
