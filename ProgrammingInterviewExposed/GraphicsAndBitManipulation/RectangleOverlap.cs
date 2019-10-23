89a504eeb8cc49eceb1abc8902ade3988f6d4d1a
Vladimir Demidov <vl.i.demidov@gmail.com>
2019-10-23 09:29:47 +0300

    Rectangle Overlap

---
+++ ProgrammingInterviewExposed/GraphicsAndBitManipulation/RectangleOverlap.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingInterviewExposed.GraphicsAndBitManipulation
+{
+    /*
+        PROBLEM You are given two rectangles, each defined by an upper-left (UL)
+        corner and a lower-right (LR) corner. Both rectangles’ edges will always be parallel
+        to the x or y axis, as shown in Figure 14-2. Write a function that determines
+        whether the two rectangles overlap. For convenience, you may use the following
+        classes:
+        class Point {
+            public int x;
+            public int y;
+            public Point( int x, int y ){
+                this.x = x;
+                this.y = y;
+            }
+        }
+        class Rect {
+            public Point ul;
+            public Point lr;
+            public Rect( Point ul, Point lr ){
+                this.ul = ul;
+                this.lr = lr;
+            }
+        }
+        The function should take two Rect objects and return true if they overlap, and
+        false if they don’t.
+     */
+
+    class RectangleOverlap
+    {
+        public static bool Overlap(Rect a, Rect b)
+        {
+            return a.lr.x >= b.ul.x && a.ul.x <= b.lr.x
+                 && a.lr.y <= b.ul.y && a.ul.y >= b.lr.y;
+        }
+
+    }
+
+    class Point
+    {
+        public int x;
+        public int y;
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Rect
+    {
+        public Point ul;
+        public Point lr;
+        public Rect(Point ul, Point lr)
+        {
+            this.ul = ul;
+            this.lr = lr;
+        }
+    }
+}
---
+++ ProgrammingInterviewExposed/GraphicsAndBitManipulation/RectangleOverlapTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProgrammingInterviewExposed.GraphicsAndBitManipulation
+{
+    public class RectangleOverlapTests
+    {
+        [Fact]
+        public void CalculatesOverlappingRight()
+        {
+            var r1 = new Rect(new Point(100, 300), new Point(200, 100));
+
+            var r2left=new Rect(new Point(10, 200), new Point(90, 100));
+            var r2leftIntersect = new Rect(new Point(10, 200), new Point(150, 100));
+            var r2Inside = new Rect(new Point(110, 200), new Point(150, 120));
+            var r2RightIntersect = new Rect(new Point(110, 200), new Point(250, 120));
+            var r2Right = new Rect(new Point(210, 200), new Point(250, 120));
+
+
+            Assert.False(RectangleOverlap.Overlap(r1, r2left));
+            Assert.True(RectangleOverlap.Overlap(r1, r2leftIntersect));
+            Assert.True(RectangleOverlap.Overlap(r1, r2Inside));
+            Assert.True(RectangleOverlap.Overlap(r1, r2RightIntersect));
+            Assert.False(RectangleOverlap.Overlap(r1, r2Right));
+        }
+    }
+}
